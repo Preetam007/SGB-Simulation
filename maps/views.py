eb063efdd9f21290c496a82e42d44ae5ea596114
Manish Lamichhane <manishlamichhane@gmail.com>
2017-04-12 15:34:40 +0300

    Exchange rate is displayed in marker content

--- maps/views.py
+++ maps/views.py
@@ -9,5 +9,7 @@ def index(request):
 	lon = request.GET.get('lon','default')
 	clat = request.GET.get('clat','default')
 	clon = request.GET.get('clon','default')
-	percent_filled = request.GET.get('percent_filled','default')
-	return render(request,"maps/index.html",{'lat':lat,'lon':lon,'clat':clat,'clon':clon,'percent_filled':percent_filled})
+	percent_filled = request.GET.get('percent_filled','0')
+	exchange = request.GET.get('exchange','0')
+	data = {'lat':lat,'lon':lon,'clat':clat,'clon':clon,'percent_filled':percent_filled, 'eur_exchange': exchange}
+	return render(request,"maps/index.html",data)
