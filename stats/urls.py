d2fd27a6b4482d495258133226f6d8d5e78f92fb
Manish Lamichhane <manishlamichhane@gmail.com>
2017-04-12 23:28:06 +0300

    Stats added. Loca Nodejs server has the api. It should be hosted online to make use of it

--- stats/urls.py
+++ stats/urls.py
@@ -13,13 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url,include
-from django.contrib import admin
+from django.conf.urls import url
+from . import views
+
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'^', include('qr_reader.urls')),
-    url(r'^events', include('events.urls')),
-    url(r'^maps/', include('maps.urls')),
-    url(r'^stats/', include('stats.urls'))
+    url(r'^$', views.index,name="stats-index"),
+    
 ]
--- stats/views.py
+++ stats/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+import urllib2
+import json
 
 # Create your views here.
+
+def index(request):
+	# make an api call here
+	
+	
+	try:
+	 	response = urllib2.urlopen('http://localhost:3000/api/account')
+	 	data = json.load(response)
+	 	return render(request,"stats/index.html",data)
+	except urllib2.HTTPError, e:
+	    print e.code
+	    return HttpResponse("<h2>Invalid API url</h2>")
+	except urllib2.URLError, e:
+	    print e.args
+	    return HttpResponse("<h2>Invalid API url</h2>")
+	
